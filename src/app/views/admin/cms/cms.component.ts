46de4461688605e61125b11c60651d3178167a9a
subodhdeshpande <subodh@arkenea.com>
2019-05-03 19:49:51 +0530

    Added dynamic list for CMS pages

--- src/app/views/admin/cms/cms.component.ts
+++ src/app/views/admin/cms/cms.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { APIService } from './../../../api.service';
+import { egretAnimations } from "../../../shared/animations/egret-animations";
 @Component({
   selector: 'cms',
   templateUrl: './cms.component.html',
   styleUrls: ['./cms.component.scss'],
+  animations: egretAnimations
 })
 export class cmslistComponent implements OnInit {
   userId: string
   closeResult: string;
-  showLoading: boolean
-  successMessage: string = ""
-  errorMessage: string = ""
   showOrgSugg: boolean = true
   checkedData: any = []
   userType: string = ""
@@ -34,86 +33,42 @@ export class cmslistComponent implements OnInit {
     this.getLists()
   }
 
-  //function to get all events
+  //function to get all cms pages
   getLists = (query = {}, search = false) => {
-    this.showLoading = false
     const req_vars = {
-      query: Object.assign({ userType: "TeamMember" }, query),
-	  fields: {},
-      offset: '',
-	  limit: '',
-	  order: {},
+      query: Object.assign({ }, query)	  
     }
-    this.api.apiRequest('post', 'userlist/list', req_vars).subscribe(result => {
+    this.api.apiRequest('post', 'cms/list', req_vars).subscribe(result => {
       if(result.status == "error"){
 		  console.log(result.data)        
       } else {
-		this.advisorlistdata = this.rows = this.temp = result.data.userList		
-		this.columns = this.getDataConf();
-       	this.totalRecords = result.data.totalUsers
+		    this.advisorlistdata = this.rows = this.temp = result.data.cmsList			
       }
     }, (err) => {
-      console.error(err)
-      this.showLoading = false
+      console.error(err)      
     })
   }
 
-    //function to hide alerts
-  hideAlert() {
-    setTimeout(()=>{
-      this.successMessage = ""
-      this.errorMessage = ""
-    },5000)
-  }
   
-  
- getDataConf(){
-    return [     
-      {
-        prop: 'fullName',
-        name: 'Name'
-      },
-      {
-        prop: 'username',
-        name: 'Email'
-      },
-      {
-        prop: 'userType',
-        name: 'Type'
-      },
-      {
-        prop: 'status',
-        name: 'Status'
-      },
-      {
-        prop: 'lastLoggedInOn',
-        name: 'Last Login date'
-      }
-    ];
-  }
-
   
 //table
   updateFilter(event) {
+    console.log("sdfkjlhsdkjfhkjh"+this.temp[0])
     const val = event.target.value.toLowerCase();
     var columns = Object.keys(this.temp[0]);
-    // Removes last "$$index" from "column"
     columns.splice(columns.length - 1);
 
-    // console.log(columns);
     if (!columns.length)
       return;
     const rows = this.temp.filter(function(d) {
       for (let i = 0; i <= columns.length; i++) {
         let column = columns[i];
-        // console.log(d[column]);
         if (d[column] && d[column].toString().toLowerCase().indexOf(val) > -1) {
           return true;
         }
       }
     });
     this.rows = rows;
-
   }
 
 
