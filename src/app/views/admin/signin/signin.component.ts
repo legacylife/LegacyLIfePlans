107aed1283d73a5791a3c1a1ad6e240de05bceab
subodhdeshpande <subodh@arkenea.com>
2019-05-06 12:16:52 +0530

    Resolved password bug in admin signin

--- src/app/views/admin/signin/signin.component.ts
+++ src/app/views/admin/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { NgModule,Component, OnInit, ViewChild } from '@angular/core';
+import { NgModule, Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { APIService } from './../../../api.service';
 import { MatProgressBar, MatButton, MatSnackBar } from '@angular/material';
@@ -17,59 +17,59 @@ export class signinComponent implements OnInit {
 
   otpsec = false;
   llpsigninForm: FormGroup;
-  constructor(private router: Router,private activeRoute: ActivatedRoute,private api: APIService,private fb: FormBuilder,  private snack: MatSnackBar, private loader: AppLoaderService) { }
+  constructor(private router: Router, private activeRoute: ActivatedRoute, private api: APIService, private fb: FormBuilder, private snack: MatSnackBar, private loader: AppLoaderService) { }
 
   ngOnInit() {
-      localStorage.clear();
-      sessionStorage.clear();
+    localStorage.clear();
+    sessionStorage.clear();
 
-	  this.llpsigninForm = new FormGroup({
-		  username: new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i)]),
-		  password: new FormControl('', Validators.required)
-		})
+    this.llpsigninForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i)]),
+      password: new FormControl('', Validators.required)
+    })
   }
 
   loginClick() {
     this.otpsec = true;
   }
-  
-  llpsignin (userData = null) {
+
+  llpsignin(userData = null) {
     let signInData = {
-      username:  this.llpsigninForm.controls['username'].value,
+      username: this.llpsigninForm.controls['username'].value,
       password: this.llpsigninForm.controls['password'].value,
       //stayLoggedIn:  this.llpsigninForm.controls['rememberMe'].value,
       userType: "sysadmin"
     }
-	this.loader.open();
+    this.loader.open();
     this.api.apiRequest('post', 'auth/signin', signInData).subscribe(result => {
-	  this.loader.close();
-      if(result.status == "success"){
+      this.loader.close();
+      if (result.status == "success") {
         userData = result.data;
-          localStorage.setItem("userId", userData.userId)
-          localStorage.setItem("userType", userData.userType)
-		  localStorage.setItem("firstName", userData.firstName)
-		  localStorage.setItem("lastName", userData.lastName)
-		  this.snack.open(result.data.message, 'OK', { duration: 4000 })
-          this.router.navigate(['/', 'admin', 'userlist'])
+        localStorage.setItem("userId", userData.userId)
+        localStorage.setItem("userType", userData.userType)
+        localStorage.setItem("firstName", userData.firstName)
+        localStorage.setItem("lastName", userData.lastName)
+        this.snack.open(result.data.message, 'OK', { duration: 4000 })
+        this.router.navigate(['/', 'admin', 'userlist'])
 
       } else {
         this.llpsigninForm.controls['username'].enable();
-		var emails = this.llpsigninForm.controls['username'].value
-      
+        var emails = this.llpsigninForm.controls['username'].value
+
         this.llpsigninForm.controls['password'].markAsUntouched();
-		this.llpsigninForm = new FormGroup({
-		  username: new FormControl('', Validators.required),
-		  password: new FormControl('', [this.customValidator])
-		})
-		this.llpsigninForm.controls['username'].setValue(emails);		
+        this.llpsigninForm = new FormGroup({
+          username: new FormControl('', Validators.required),
+          password: new FormControl('', Validators.required)
+        })
+        this.llpsigninForm.controls['username'].setValue(emails);
       }
     }, (err) => {
-      
+
     })
   }
-  
-   public customValidator(control: FormControl) {
-  	 return { 'invalid': true };
+
+  public customValidator(control: FormControl) {
+    return { 'invalid': true };
   }
 
 }
