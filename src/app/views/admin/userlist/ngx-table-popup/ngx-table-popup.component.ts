2c9ba47a7b93420a7ebea5b7cff97e1c533f865c
subodhdeshpande <subodh@arkenea.com>
2019-05-06 21:15:38 +0530

    Added code to add new admin user

--- src/app/views/admin/userlist/ngx-table-popup/ngx-table-popup.component.ts
+++ src/app/views/admin/userlist/ngx-table-popup/ngx-table-popup.component.ts
@@ -9,6 +9,7 @@ import { APIService } from './../../../../api.service';
 })
 export class NgxTablePopupComponent implements OnInit {
   public itemForm: FormGroup;
+  adminSections = [];
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<NgxTablePopupComponent>,
@@ -17,19 +18,80 @@ export class NgxTablePopupComponent implements OnInit {
 
   ngOnInit() {
     this.buildItemForm(this.data.payload)
+
+    this.adminSections = [{
+      name: 'User Management',
+      code: "usermanagement"
+    }, {
+      name: 'Advisor Management',
+      code: "advisormanagement"
+    }, {
+      name: 'Activity Log',
+      code: "activitylog"
+    }, {
+      name: 'Zip Code map',
+      code: "zipcodemap"
+    }, {
+      name: 'CMS pages',
+      code: "cms"
+    },{
+      name: 'Referral program',
+      code: "referral"
+    }, {
+      name: 'Advertisement management',
+      code: "addmanagement"
+    }, {
+      name: 'Deceased requests',
+      code: "deceasedrequest"
+    }, {
+      name: 'Admin Management',
+      code: "adminmanagement"
+    }]
+
+
+
   }
   buildItemForm(item) {
   console.log('buildItemForm')
+  
     this.itemForm = this.fb.group({
-      fullName: [item.fullName || '', Validators.required],
+      firstName: [item.firstName || '', Validators.required],
+      lastName: [item.lastName || '', Validators.required],
       username: [item.username || '', Validators.required],
-      status: [item.status || false]
+      status: [item.status || false],
+      usermanagement: [item.usermanagement || '', Validators.required],
+      advisormanagement: [item.advisormanagement || '', Validators.required],
+      activitylog: [item.activitylog || '', Validators.required],
+      zipcodemap: [item.zipcodemap || '', Validators.required],
+      cms: [item.cms || '', Validators.required],
+      referral: [item.referral || '', Validators.required],
+      addmanagement: [item.addmanagement || '', Validators.required],
+      deceasedrequest: [item.deceasedrequest || '', Validators.required],
+      adminmanagement: [item.adminmanagement || '', Validators.required]
     })
   }
 
   submit() {
-	  console.log(this.itemForm.value);
-   this.api.apiRequest('post', 'userlist/addmember', this.itemForm.value).subscribe(result => {
+    let RequestData = {
+      firstName: this.itemForm.controls['firstName'].value,
+      lastName: this.itemForm.controls['lastName'].value,
+      username: this.itemForm.controls['username'].value,
+      status: this.itemForm.controls['status'].value,      
+      userType: "sysadmin",      
+      sectionAccess : {
+        "usermanagement" : this.itemForm.controls['usermanagement'].value,
+        "advisormanagement" : this.itemForm.controls['advisormanagement'].value,
+        "activitylog" : this.itemForm.controls['activitylog'].value,
+        "zipcodemap" : this.itemForm.controls['zipcodemap'].value,
+        "cms" : this.itemForm.controls['cms'].value,
+        "referral" : this.itemForm.controls['referral'].value,
+        "addmanagement" : this.itemForm.controls['addmanagement'].value,
+        "deceasedrequest" : this.itemForm.controls['deceasedrequest'].value,
+        "adminmanagement" : this.itemForm.controls['adminmanagement'].value
+      }
+    }
+   
+   this.api.apiRequest('post', 'userlist/addmember', RequestData).subscribe(result => {
       if(result.status == "error"){
 		 this.dialogRef.close(this.itemForm.value)
       } else {
