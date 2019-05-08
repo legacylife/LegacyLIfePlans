3c081db1c64b202332d8c836c9275b927516deca
subodhdeshpande <subodh@arkenea.com>
2019-05-08 17:02:08 +0530

    Added dev server URL

--- config/constants.js
+++ config/constants.js
@@ -23,8 +23,8 @@ var constants = {
     secret: "GI/4d3sT5WtV4Rg/QtxRPVo2RsRn71PH/ZdKHt99",
     fromEmail: "subodh@arkenea.com",
   },
-  clientUrl: process.env.clientUrl || 'http://ec2-3-212-172-15.compute-1.amazonaws.com:8080',
-  mailServerUrl : process.env.mailServerUri || 'http://ec2-3-212-172-15.compute-1.amazonaws.com:8080',
+  clientUrl: process.env.clientUrl || 'http://localhost:4200',
+  mailServerUrl : process.env.mailServerUri || 'http://localhost:8080',
   socialMedia: {
     facebook: {
       clientId: ''
