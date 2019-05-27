c0a07e72cc27bdfe041ad040a75213ed8a2e3c0b
subodhdeshpande <subodh@arkenea.com>
2019-05-27 10:15:31 +0530

    Added code for advisor account setting

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
 var constants = {
   secret: "LLP",
-  database: {
-    url: process.env.dbURI || 'mongodb://llp:llp#123@ds213896.mlab.com:13896/llp-pankaj'
-  },
+  database: { // mongodb://localhost:27017/llp
+    url: process.env.dbURI || 'mongodb://llp:llp#123@ds129454.mlab.com:29454/llp'
+  }, // mongodb://llp:llp#123@ds129454.mlab.com:29454/llp
   google: {
   },
   nylas:{
@@ -23,8 +23,8 @@ var constants = {
     secret: "GI/4d3sT5WtV4Rg/QtxRPVo2RsRn71PH/ZdKHt99",
     fromEmail: "subodh@arkenea.com",
   },
-  clientUrl: process.env.clientUrl || 'http://localhost:4200',
-  mailServerUrl : process.env.mailServerUri || 'http://localhost:8080',
+  clientUrl: process.env.clientUrl || 'http://ec2-3-212-172-15.compute-1.amazonaws.com:8080',
+  mailServerUrl : process.env.mailServerUri || 'http://ec2-3-212-172-15.compute-1.amazonaws.com:8080',
   socialMedia: {
     facebook: {
       clientId: ''
