5c15273d2c4c3dd0d7ded74dde2ae6bc10ab15ef
Pankaj K <subodhdeshpande40@gmail.com>
2019-06-10 15:10:31 +0530

    My essentials forms integration

---
+++ src/app/country.ts
@@ -0,0 +1,251 @@
+export const countries = [{ 
+  "country_name" : "Alabama", 
+  "short_code" : "AL"
+},
+{ 
+  "country_name" : "Alaska", 
+  "short_code" : "AK"
+},
+{ 
+  "country_name" : "American Samoa", 
+  "short_code" : "AS"
+},
+{ 
+  "country_name" : "Arizona", 
+  "short_code" : "AZ"
+},
+{ 
+  "country_name" : "Arkansas", 
+  "short_code" : "AR"
+},
+{ 
+  "country_name" : "Armed Forces (the) Americas", 
+  "short_code" : "AA"
+},
+{ 
+  "country_name" : "Armed Forces Europe", 
+  "short_code" : "AE"
+},
+{ 
+  "country_name" : "Armed Forces Pacific", 
+  "short_code" : "AP"
+},
+{ 
+  "country_name" : "California", 
+  "short_code" : "CA"
+},
+{ 
+  "country_name" : "Colorado", 
+  "short_code" : "CO"
+},
+{ 
+  "country_name" : "Connecticut", 
+  "short_code" : "CT"
+},
+{ 
+  "country_name" : "Delaware", 
+  "short_code" : "DE"
+},
+{ 
+  "country_name" : "District of Columbia (DC)", 
+  "short_code" : "DC"
+},
+{ 
+  "country_name" : "Federated States of Micronesia", 
+  "short_code" : "FM"
+},
+{ 
+  "country_name" : "Florida", 
+  "short_code" : "FL"
+},
+{ 
+  "country_name" : "Georgia", 
+  "short_code" : "GA"
+},
+{ 
+  "country_name" : "Guam", 
+  "short_code" : "GU"
+},
+{ 
+  "country_name" : "Hawaii", 
+  "short_code" : "HI"
+},
+{ 
+  "country_name" : "Idaho", 
+  "short_code" : "ID"
+},
+{ 
+  "country_name" : "Illinois", 
+  "short_code" : "IL"
+},
+{ 
+  "country_name" : "Indiana", 
+  "short_code" : "IN"
+},
+{ 
+  "country_name" : "Iowa", 
+  "short_code" : "IA"
+},
+{ 
+  "country_name" : "Kansas", 
+  "short_code" : "KS"
+},
+{ 
+  "country_name" : "Kentucky", 
+  "short_code" : "KY"
+},
+{ 
+  "country_name" : "Louisiana", 
+  "short_code" : "LA"
+},
+{ 
+  "country_name" : "Maine", 
+  "short_code" : "ME"
+},
+{ 
+  "country_name" : "Marshall Islands", 
+  "short_code" : "MH"
+},
+{ 
+  "country_name" : "Maryland", 
+  "short_code" : "MD"
+},
+{ 
+  "country_name" : "Massachusetts", 
+  "short_code" : "MA"
+},
+{ 
+  "country_name" : "Michigan", 
+  "short_code" : "MI"
+},
+{ 
+  "country_name" : "Minnesota", 
+  "short_code" : "MN"
+},
+{ 
+  "country_name" : "Mississippi", 
+  "short_code" : "MS"
+},
+{ 
+  "country_name" : "Missouri", 
+  "short_code" : "MO"
+},
+{ 
+  "country_name" : "Montana", 
+  "short_code" : "MT"
+},
+{ 
+  "country_name" : "Nebraska", 
+  "short_code" : "NE"
+},
+{ 
+  "country_name" : "Nevada", 
+  "short_code" : "NV"
+},
+{ 
+  "country_name" : "New Hampshire", 
+  "short_code" : "NH"
+},
+{ 
+  "country_name" : "New Jersey", 
+  "short_code" : "NJ"
+},
+{ 
+  "country_name" : "New Mexico", 
+  "short_code" : "NM"
+},
+{ 
+  "country_name" : "New York", 
+  "short_code" : "NY"
+},
+{ 
+  "country_name" : "North Carolina", 
+  "short_code" : "NC"
+},
+{ 
+  "country_name" : "North Dakota", 
+  "short_code" : "ND"
+},
+{ 
+  "country_name" : "Northern Mariana Islands", 
+  "short_code" : "MP"
+},
+{ 
+  "country_name" : "Ohio", 
+  "short_code" : "OH"
+},
+{ 
+  "country_name" : "Oklahoma", 
+  "short_code" : "OK"
+},
+{ 
+  "country_name" : "Oregon", 
+  "short_code" : "OR"
+},
+{ 
+  "country_name" : "Palau", 
+  "short_code" : "PW"
+},
+{ 
+  "country_name" : "Pennsylvania", 
+  "short_code" : "PA"
+},
+{ 
+  "country_name" : "Puerto Rico", 
+  "short_code" : "PR"
+},
+{ 
+  "country_name" : "Rhode Island", 
+  "short_code" : "RI"
+},
+{ 
+  "country_name" : "South Carolina", 
+  "short_code" : "SC"
+},
+{ 
+  "country_name" : "South Dakota", 
+  "short_code" : "SD"
+},
+{ 
+  "country_name" : "Tennessee", 
+  "short_code" : "TN"
+},
+{ 
+  "country_name" : "Texas", 
+  "short_code" : "TX"
+},
+{ 
+  "country_name" : "Utah", 
+  "short_code" : "UT"
+},
+{ 
+  "country_name" : "Vermont", 
+  "short_code" : "VT"
+},
+{ 
+  "country_name" : "Virgin Islands", 
+  "short_code" : "VI"
+},
+
+{ 
+  "country_name" : "Virginia", 
+  "short_code" : "VA"
+},
+{ 
+  "country_name" : "Washington", 
+  "short_code" : "WA"
+},
+{ 
+  "country_name" : "West Virginia", 
+  "short_code" : "WV"
+},
+{ 
+  "country_name" : "Wisconsin", 
+  "short_code" : "WI"
+},
+{ 
+  "country_name" : "Wyoming", 
+  "short_code" : "WY"
+}
+
+]
--- src/app/views/customer/customer-home/personal-profile-modal/personal-profile-modal.component.ts
+++ src/app/views/customer/customer-home/personal-profile-modal/personal-profile-modal.component.ts
@@ -1,12 +1,11 @@
-
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 import { CustomValidators } from 'ng2-validation';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { MatStepperModule , MatStepper} from '@angular/material/stepper';
 import { Router, ActivatedRoute } from '@angular/router';
 import { forEach } from "lodash";
-
+import { countries } from '../../../../country';
 
 
 @Component({
@@ -19,19 +18,73 @@ export class PersonalProfileModalComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder) {}
+  userId: string;
+  countryList: any;
+  country_name: string;
+  short_code: string;
+  ppemails: any;
+  landlineNumbers: any;
+  wpLandlineNumbers: any;
+  ccWorkLandlineNumbers:any; 
+  ccChurchLandlineNumbers:any; 
+  constructor(private snack: MatSnackBar, public dialog: MatDialog,private fb: FormBuilder, ) {}
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.countryList = countries;
+
+    this.userId = localStorage.getItem("endUserId");
+//this.firstFormGroup = new FormGroup({
+
+  this.firstFormGroup = this.fb.group({
+      ppFirstName: new FormControl('', Validators.required),
+      ppMiddleName: new FormControl('', Validators.required),
+      ppLastName: new FormControl('', Validators.required),
+      ppEmails: this.fb.array([this.fb.group({email: ['', Validators.required]})]),
+      landlineNumbers: this.fb.array([this.fb.group({phone: ['', Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]})]),
+      //phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]),
+     // landlineNumbers: new FormControl('', [Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]),
+      ppDateOfBirth: new FormControl('', ),      
+      ppAddressLine1: new FormControl('', Validators.required),
+      ppAddressLine2: new FormControl(''),
+      ppCountry: new FormControl('', Validators.required),
+      ppCity: new FormControl('', Validators.required),
+      ppState: new FormControl('', Validators.required),
+      ppZipCode: new FormControl('', [Validators.required, , Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)])
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-    this.thirdFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+ //ppEmails   this.emailList this.firstFormGroup.controls['firstName'].setValue(this.profile.firstName ? this.profile.firstName : "");
+
+  this.secondFormGroup = this.fb.group({
+    wpWorkBusiness: new FormControl('', Validators.required),
+    wpCompanyName: new FormControl('', Validators.required),
+    wpTitlePosition: new FormControl('', Validators.required),
+    wpDepartment: new FormControl('', Validators.required),
+    wpContactPersonName: new FormControl('', Validators.required),
+    wpLandlineNumbers: this.fb.array([this.fb.group({phone: ['', Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]})]),
+    wpAddressLine1: new FormControl('', Validators.required),
+    wpAddressLine2: new FormControl(''),
+    wpCountry: new FormControl('', Validators.required),
+    wpCity: new FormControl('', Validators.required),
+    wpState: new FormControl('', Validators.required),
+    wpZipCode: new FormControl('', [Validators.required, , Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)])
+  });
+
+
+  this.thirdFormGroup = this.fb.group({
+    ccName: new FormControl('', Validators.required),
+    ccAddressLine1: new FormControl('', Validators.required),
+    ccAddressLine2: new FormControl(''),
+    ccZipCode: new FormControl('', [Validators.required, , Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)]),
+    ccWorkLandlineNumbers: this.fb.array([this.fb.group({phone: ['', Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]})]),
+    ccContactPersonName: new FormControl('', Validators.required),            
+    cclandlineNumbers: this.fb.array([this.fb.group({phone: ['', Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]})]),
+    ccChurchName: new FormControl('', Validators.required),            
+    ccChurchAddressLine1: new FormControl('', Validators.required),
+    ccChurchAddressLine2: new FormControl(''),      
+    ccChurchZipCode: new FormControl('', [Validators.required, Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)]),
+    ccChurchLandlineNumbers: this.fb.array([this.fb.group({phone: ['', Validators.required, Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]})]),    
+    ccChurchContactPersonName: new FormControl('', Validators.required),
+  });
+
   }
 
   onChangeFormIndex(event){
@@ -49,6 +102,93 @@ export class PersonalProfileModalComponent implements OnInit {
     });
 
   }
+
+  addNewEmail() {
+    this.emailList.push(this.fb.group({
+      email: ['', Validators.required]
+    }));
+  }
+
+  get emailList() {  
+    return this.firstFormGroup.get('ppEmails') as FormArray;
+  }
+
+/////
+  addNewLandlineNum() {
+    this.landlineNumbersList.push(this.fb.group({
+      phone: ['', Validators.required,Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]
+    }));
+  }
+
+  get landlineNumbersList() {  
+    return this.firstFormGroup.get('landlineNumbers') as FormArray;
+  }
+
+/////
+  addNewWorkLandLineNum() {
+    this.wpLandlineNumberList.push(this.fb.group({
+      phone: ['', Validators.required,Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]
+    }));
+  }
+
+  get wpLandlineNumberList() {  
+    return this.secondFormGroup.get('wpLandlineNumbers') as FormArray;
+  }
+
+  /////
+  addNewWorkLandlineNum() {
+    this.ccWorkLandlineNumberList.push(this.fb.group({
+      phone: ['', Validators.required,Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]
+    }));
+  }
+
+  get ccWorkLandlineNumberList() {  
+    return this.thirdFormGroup.get('ccWorkLandlineNumbers') as FormArray;
+  }
+  
+  /////
+  addNewCCLandlineNum() {
+    this.cclandlineNumbersList.push(this.fb.group({
+      phone: ['', Validators.required,Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]
+    }));
+  }
+
+  get cclandlineNumbersList() {  
+    return this.thirdFormGroup.get('cclandlineNumbers') as FormArray;
+  }
+
+  /////
+  addNewChurchLandlineNum() {
+    this.ChurchlandlineNumbersList.push(this.fb.group({
+      phone: ['', Validators.required,Validators.pattern(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)]
+    }));
+  }
+
+  get ChurchlandlineNumbersList() {  
+    return this.thirdFormGroup.get('ccChurchLandlineNumbers') as FormArray;
+  }
+  
+  
+  // all add new fields function
+  delete(i,arryName) {
+    const control = <FormArray>this.firstFormGroup.controls[arryName];
+    control.removeAt(i);
+  }
+
+  deletewp(i,arryName) {
+    const control = <FormArray>this.secondFormGroup.controls[arryName];
+    control.removeAt(i);
+  }
+
+  deletecc(i,arryName) {
+    const control = <FormArray>this.thirdFormGroup.controls[arryName];
+    control.removeAt(i);
+  }
+
+
+
+
+
 }
 
 
