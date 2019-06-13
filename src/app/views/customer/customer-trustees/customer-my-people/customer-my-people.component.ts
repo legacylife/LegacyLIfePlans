30b7b7342aa8e8ddb9da7318d940b413db6ec2df
Prashant Thorat <prashant.thorat@arkenea.com>
2019-06-13 15:30:54 +0530

    Completed trustee landing page with dynamic

--- src/app/views/customer/customer-trustees/customer-my-people/customer-my-people.component.ts
+++ src/app/views/customer/customer-trustees/customer-my-people/customer-my-people.component.ts
@@ -14,8 +14,90 @@ import { egretAnimations } from '../../../../shared/animations/egret-animations'
   animations: [egretAnimations]
 })
 export class CustomerMyPeopleComponent implements OnInit {
+  allPeoples: any[];
+
   constructor(
   ) { }
+
+
   ngOnInit() {
+    this.allPeoples = [
+      {
+        profilePic: 'assets/images/arkenea/ca.jpg',
+        userName: 'Allen Barry',
+        emailId: 'barryallen@gmail.com',
+        totalFiles: '24 Files',
+        totalFolders: '9 Folders',
+        position: 'CFA, CIC',
+        status: 'assigned'
+      },
+      {
+        profilePic: 'assets/images/arkenea/emily.png',
+        userName: 'Emily Doe',
+        emailId: 'emilydoe@gmail.com',
+        totalFiles: '4 Files',
+        totalFolders: '1 Folders',
+        position: 'CFA, CIC',
+        status: 'pending'
+      },
+      {
+        profilePic: 'assets/images/arkenea/john.png',
+        userName: 'Johnson Smith',
+        emailId: 'johnson.smith@gmail.com',
+        totalFiles: '15 Files',
+        totalFolders: '6 Folders',
+        position: 'CFA, CIC',
+        status: 'advisor'
+      },
+      {
+        profilePic: 'assets/images/arkenea/user-male.png',
+        userName: 'James Anderson',
+        emailId: 'james.anderson@gmail.com',
+        totalFiles: '15 Files',
+        totalFolders: '6 Folders',
+        position: 'CFA, CIC',
+        status: 'assigned'
+      },
+      {
+        profilePic: 'assets/images/arkenea/ca.jpg',
+        userName: 'Allen Barry',
+        emailId: 'barryallen@gmail.com',
+        totalFiles: '24 Files',
+        totalFolders: '9 Folders',
+        position: 'CFA, CIC',
+        status: 'assigned'
+      },
+      {
+        profilePic: 'assets/images/arkenea/emily.png',
+        userName: 'Emily Doe',
+        emailId: 'emilydoe@gmail.com',
+        totalFiles: '4 Files',
+        totalFolders: '1 Folders',
+        position: 'CFA, CIC',
+        status: 'pending'
+      },
+      {
+        profilePic: 'assets/images/arkenea/john.png',
+        userName: 'Johnson Smith',
+        emailId: 'johnson.smith@gmail.com',
+        totalFiles: '15 Files',
+        totalFolders: '6 Folders',
+        position: 'CFA, CIC',
+        status: 'advisor'
+      },
+      {
+        profilePic: 'assets/images/arkenea/user-male.png',
+        userName: 'James Anderson',
+        emailId: 'james.anderson@gmail.com',
+        totalFiles: '15 Files',
+        totalFolders: '6 Folders',
+        position: 'CFA, CIC',
+        status: 'assigned'
+      },
+    ];
   }
+
+
+
+
 }
