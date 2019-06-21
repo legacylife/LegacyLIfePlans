2bf9c54e72126db2aa3270550f90ded6f96fbceb
subodhdeshpande <subodh@arkenea.com>
2019-06-21 12:56:13 +0530

    added staging server settings

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
 var constants = {
   secret: "LLP",
   database: { // mongodb://localhost:27017/llp
-    url: process.env.dbURI || 'mongodb://llp:llp#123@ds213896.mlab.com:13896/llp-pankaj'
-  }, // mongodb://llp:llp#123@ds129454.mlab.com:29454/llp
+    url: process.env.dbURI || 'mongodb://llp:l$l!p#123@ds341557.mlab.com:41557/llp'
+  }, // mongodb://llp:llp#123@ds129454.mlab.com:29454/llp, mongodb://llp:l$l!p#123@ds341557.mlab.com:41557/llp
   google: {
   },
   nylas:{
@@ -10,8 +10,8 @@ var constants = {
     appSecret: "27g7mvf6f606237rhxvsi21v8",
   },
   s3Details: {
-    url : "https://s3.amazonaws.com/llp-dev",
-    bucketName: "llp-dev",
+    url : "https://s3.amazonaws.com/llp-staging",
+    bucketName: "llp-staging",
     awsKey:"AKIAUPQ3GZ6WDCCRWVY5",
     awsSecret:"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",
     profilePicturesPath:"profilePictures/",
@@ -19,15 +19,15 @@ var constants = {
     myEssentialsDocumentsPath:"myEssentials/",
     legalStuffDocumentsPath:"legalStuff/",
     finalWishesFilePath:"finalWishes/",
-    serveUrl: "https://llp-dev.s3.amazonaws.com"
+    serveUrl: "https://llp-staging.s3.amazonaws.com"
   },
   ses: {
     key: "AKIAUPQ3GZ6WJFHZAMNB",
     secret: "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",
     fromEmail: "subodh@arkenea.com",
   },
-  clientUrl: process.env.clientUrl || 'http://localhost:4200',
-  mailServerUrl : process.env.mailServerUri || 'http://localhost:8080',
+  clientUrl: process.env.clientUrl || 'http://ec2-3-209-230-58.compute-1.amazonaws.com:4200',
+  mailServerUrl : process.env.mailServerUri || 'http://ec2-3-209-230-58.compute-1.amazonaws.com:8080',
   socialMedia: {
     facebook: {
       clientId: ''
