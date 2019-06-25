bcd00834829d2f16facc4a3d93bb21af04bdd734
subodhdeshpande <subodh@arkenea.com>
2019-06-25 13:07:29 +0530

    Added code to manage access recent file section

--- src/app/views/customer/customer-home/customer-dashboard-day-one/customer-dashboard-day-one.component.ts
+++ src/app/views/customer/customer-home/customer-dashboard-day-one/customer-dashboard-day-one.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms'
 import { Subscription, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { egretAnimations } from '../../../../shared/animations/egret-animations';
+import { UserAPIService } from './../../../../userapi.service';
 
 @Component({
   selector: 'app-customer-home',
@@ -15,17 +16,48 @@ import { egretAnimations } from '../../../../shared/animations/egret-animations'
 export class CustomerDashboardDayOneComponent implements OnInit {
 
   @ViewChild(MatSidenav) private sideNav: MatSidenav;
+  recentlyAccessedFiles : boolean = false;
+  userId: string;
+  fileActivityLogList:any;
 
 
 
   constructor(
     private fb: FormBuilder,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private userapi: UserAPIService
   ) { }
 
 
   ngOnInit() {
+    this.userId = localStorage.getItem("endUserId");
+    this.getFileActivityLogList();
+  }
 
+  getFileActivityLogList = (query = {}, search = false) => {
+    const req_vars = {
+      query: Object.assign({ customerId: this.userId }, query),
+      fields: {},
+      offset: 0,
+      limit: 6,
+      order: {"modifiedOn": -1},
+    }   
+    this.userapi.apiRequest('post', 'customer/file-activity-log-list', req_vars).subscribe(result => {  
+      if (result.status == "error") {
+        console.log(result.data)
+      } else {
+        this.fileActivityLogList = result.data.activityLogList;        
+        if(result.data.totalRecords > 0){
+          this.recentlyAccessedFiles = true;
+        }        
+      }
+    }, (err) => {
+      console.error(err);
+    })
+  }
+
+  getIconNUrl(logData){
+    return this.userapi.getFileIconNUrl(logData);
   }
 
 
