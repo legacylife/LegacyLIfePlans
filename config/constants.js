721a95a404a5d43ca9cff7b76cf32e6947b412f3
subodhdeshpande <subodh@arkenea.com>
2019-07-02 19:47:16 +0530

    config changes

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 var constants = {
   secret: "LLP",
   database: { // mongodb://localhost:27017/llp
-    url: process.env.dbURI || 'mongodb://llp:l$l!p#123@ds341557.mlab.com:41557/llp'
+    url: process.env.dbURI || 'mongodb://llp:llp#123@ds213896.mlab.com:13896/llp-pankaj'
   }, // mongodb://llp:llp#123@ds129454.mlab.com:29454/llp, mongodb://llp:l$l!p#123@ds341557.mlab.com:41557/llp  mongodb://llp:llp#123@ds213896.mlab.com:13896/llp-pankaj
   google: {
   },
@@ -33,8 +33,8 @@ var constants = {
     secret: "GI/4d3sT5WtV4Rg/QtxRPVo2RsRn71PH/ZdKHt99",
     fromEmail: "subodh@arkenea.com",
   },
-  clientUrl: process.env.clientUrl || 'http://ec2-3-209-230-58.compute-1.amazonaws.com:4200',
-  mailServerUrl : process.env.mailServerUri || 'http://ec2-3-209-230-58.compute-1.amazonaws.com',
+  clientUrl: process.env.clientUrl || 'http://localhost:4200',
+  mailServerUrl : process.env.mailServerUri || 'http://localhost:8080',
   socialMedia: {
     facebook: {
       clientId: ''
