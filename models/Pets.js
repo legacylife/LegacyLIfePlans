53664b888623fb97ff04ad9d97938bd2cc994e45
manojn-arkenea <manojn@arkenea.com>
2019-07-10 12:02:19 +0530

    working on legaices module

--- models/Pets.js
+++ models/Pets.js
@@ -3,6 +3,8 @@ var constants = require("./../config/constants")
 
 var petsSchema = new mongoose.Schema({
   customerId: String,
+  customerLegacyId: String,
+  customerLegacyType: String,
   name: String,
   petType: String,
   veterinarian : String,
