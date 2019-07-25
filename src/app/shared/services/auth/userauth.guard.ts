0e8715811dde1d0c6daa763fc3e361c94356a568
nilu4179 <nileshy@arkenea.com>
2019-07-25 21:08:55 +0530

    account setting jira issues fixed

--- src/app/shared/services/auth/userauth.guard.ts
+++ src/app/shared/services/auth/userauth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, ActivatedRoute } from '@angular/router';
 import { UserAPIService } from './../../../userapi.service';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class UserAuthGuard implements CanActivate {
   private urlData: any
   private userUrlType: any
   
-  constructor(private router: Router, private userapi: UserAPIService) {}
+  constructor(private router: Router, private userapi: UserAPIService,private route: ActivatedRoute) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -19,10 +19,16 @@ export class UserAuthGuard implements CanActivate {
       let isProuser = localStorage.getItem('endUserProSubscription') && localStorage.getItem('endUserProSubscription') == 'yes' ? true : false
       var pathArray = window.location.pathname.split('/');
       this.userUrlType = pathArray[1];
-      console.log("this.userUrlType",this.userUrlType)
-      /* if( !isProuser && this.userUrlType != 'account-setting' ) {
-        this.router.navigate(['/'+this.userInfo.endUserType+'/account-setting']);
-        return false;
+      console.log("this.userUrlType",pathArray[2],"asdasdasda",this.userInfo.endUserType,"isProuser",isProuser)
+      /* if( this.userInfo.endUserType == 'advisor' && !isProuser ) {
+        if( pathArray[2] && pathArray[2] != 'account-setting') {
+          this.router.navigate(['/'+this.userInfo.endUserType+'/account-setting']);
+          return false;
+        }
+        else{
+          this.router.navigate(['/'+this.userInfo.endUserType+'/account-setting']);
+          return false;
+        }
       } */
       if ((this.userInfo && this.userInfo.endUserType == '') || (this.userUrlType != 'signin' && this.userInfo.endUserType != this.userUrlType)) {
         this.router.navigateByUrl('/signin');
