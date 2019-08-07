3e3c3517fd722176e83b15673a0270579f6e0e77
nilu4179 <nileshy@arkenea.com>
2019-08-07 16:38:44 +0530

    regression issues

---
+++ helpers/responseMessages.js
@@ -0,0 +1,187 @@
+let responseArray = [
+    {
+        "code":601,
+        "type":"done",
+        "message":"Done",
+    },
+    {
+        "code":602,
+        "type":"fail",
+        "message":"Failed",
+    },
+    {
+        "code":603,
+        "type":"response-failed",
+        "message":"Something went wrong",
+    },
+    {
+        "code":604,
+        "type":"server-error",
+        "message":"Some error Occured",
+    },
+    {
+        "code":605,
+        "type":"no-record",
+        "message":"Record not found.",
+    },
+    {
+        "code":606,
+        "type":"user-not-found",
+        "message":"Looks like your account does not exist. Sign up to create an account.",
+    },
+    {
+        "code":607,
+        "type":"successful-action",
+        "message":"{field} has been {status} successfully", //{status} => sent, added, saved, updated, deleted, removed
+    },
+    {
+        "code":608,
+        "type":"account-pending",
+        "message":"Your account is under review, you will receive the account confirmation email on your registered email id.",
+    },
+    {
+        "code":609,
+        "type":"account-deactivate",
+        "message":"Your account has been deactivated. Please connect with the admin at {support_email} for any queries.",
+    },
+    {
+        "code":610,
+        "type":"account-rejected",
+        "message":"Your account is rejected by admin. Please connect with the admin at support@legacylifeplans.com for any queries.",
+    },
+    {
+        "code":611,
+        "type":"account-blocked",
+        "message":"Your account has been blocked. Please connect with the admin at {support_email} for any queries.",
+    },
+    {
+        "code":612,
+        "type":"account-not-verified",
+        "message":"Your account has been not verified. Please connect with the admin at {support_email} for any queries.",
+    },
+    {
+        "code":613,
+        "type":"email-not-verified",
+        "message":"Your email has not verified. Please connect with the admin at {support_email} for any queries.",
+    },
+    {
+        "code":614,
+        "type":"incorrect-field",
+        "message":"Incorrect {field}. Please try again.",
+    },
+    {
+        "code":615,
+        "type":"incorrect-login-details",
+        "message":"Incorrect email or password. Please try again."
+    },
+    {
+        "code":616,
+        "type":"password-instructions",
+        "message":"We have sent you the instructions to set your account password. Please check your mail.",
+    },
+    {
+        "code":617,
+        "type":"password-set",
+        "message":"We have set your password. Please login & use system.",
+    },
+    {
+        "code":618,
+        "type":"invalid-credentials",
+        "message":"Invalid login credentials.",
+    },
+    {
+        "code":619,
+        "type":"all-required-field",
+        "message":"Please fill all required details.",
+    },
+    {
+        "code":620,
+        "type":"required-field",
+        "message":"Please fill {field} details.",
+    },
+    {
+        "code":621,
+        "type":"login-successful",
+        "message":"Successfully logged in!",
+    },
+    {
+        "code":622,
+        "type":"email-verification",
+        "message":"Email verification successful. Logging in to your account",
+    },
+    {
+        "code":623,
+        "type":"email-exists",
+        "message":"This email id is already taken by another user.",
+    },
+    {
+        "code":624,
+        "type":"change-email-exists",
+        "message":"Looks like you already have an account registered with this email. Please log in to access your account.",
+    },
+    {
+        "code":625,
+        "type":"add-user-email",
+        "message":"Looks like email id already have an account registered with this email as {userType}.",
+    },
+    {
+        "code":626,
+        "type":"logout-successful",
+        "message":"You have logout! Please signin again",
+    },
+    {
+        "code":627,
+        "type":"valid-field-details",
+        "message":"Please enter the valid {field} details",
+    },
+    {
+        "code":628,
+        "type":"invalid-link",
+        "message":"Invalid link, Please try again.",
+    },
+    {
+        "code":629,
+        "type":"otp-sent",
+        "message":"A new OTP is sent on your email.",
+    },
+    {
+        "code":630,
+        "type":"customer-signup",
+        "message":"Welcome to Legacy Life Plans. Your account credentials are successfully saved.",
+    },
+    {
+        "code":631,
+        "type":"adviser-signup",
+        "message":"You have successfully signup. Please update your profile.",
+    },
+    {
+        "code":632,
+        "type":"not-subscribed",
+        "message":"Not subscribed any plan yet",
+    },
+    {
+        "code":633,
+        "type":"subscription-canceled",
+        "message":"You already canceled the subscription.",
+    },
+    {
+        "code":634,
+        "type":"subscription-successful",
+        "message":"Congratulations! You are successfully subscribed the plan.",
+    },
+    {
+        "code":635,
+        "type":"unauthorize-access",
+        "message":"Unauthorize access",
+    },
+    {
+        "code":636,
+        "type":"request-sent",
+        "message":"Request sent successfully!",
+    },
+    {
+        "code":637,
+        "type":"user-details-sent",
+        "message":"The {field} details are sent on your email.",
+    },
+]
