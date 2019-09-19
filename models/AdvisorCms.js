46bea77d713b69b2ed0d00af1776e1835ecc9321
Pankaj K <subodhdeshpande40@gmail.com>
2019-09-19 11:19:29 +0530

    landing page in process

---
+++ models/AdvisorCms.js
@@ -0,0 +1,65 @@
+var mongoose = require( 'mongoose' )
+var constants = require("./../config/constants")
+
+var advisorCmsSchema = new mongoose.Schema({
+  pageFor: String,
+  sectionOne: {
+    title:String,
+    middleTitle:String,
+    subTitle:String,
+    topBanner:String,
+  },
+  sectionTwo: [{
+    title:String,
+    subTitle:String,
+  }],
+  sectionThree: {
+    title:String,
+    subTitle:String,
+    bannerImage:String,
+  },
+  sectionFour: {
+    title:String,
+    subTitle:String,
+    bannerImage:String,
+  },
+  sectionFive: [{
+    title:String,
+    subTitle:String,
+  }],
+  facts: {
+    title : String, 
+    subTitle : String,
+    savedFiles : String, 
+    trustedAdvisors : String,
+    LLPMembers : String,
+    referralConnection : String
+  },
+  featuredAdvisors:[{
+    title:String,
+    subTitle:String,
+    name:String,
+    certifications:String,
+    profilePhoto:String,
+  }],
+  sectionEight: {
+    title:String,
+    subTitle:String,
+    bannerImage:String,    
+    benefitsPoints: [{name:String}],
+  },
+  testimonials: [{
+    name:String,
+    profilePhoto:String,
+    certifications:String,
+    comment:String,
+  }],
+  status: String,
+  createdOn: Date,
+  modifiedOn: Date
+})
+
+
+
+
+module.exports = mongoose.model('advisorcms', advisorCmsSchema)
---
+++ models/CustomerCms.js
@@ -0,0 +1,48 @@
+var mongoose = require( 'mongoose' )
+var constants = require("./../config/constants")
+
+var customerCmsSchema = new mongoose.Schema({
+  pageFor: String,
+  pageTitle: String,
+  pagesubTitle: String,  
+  topBanner: String,
+  middleBanner: String,
+  middleTitle: String,
+  middleText: String,
+  lowerBanner: String,
+  lowerTitle: String,
+  lowerText: String,
+  bulletPoints: [{name:String}],
+  facts: {
+    title : String, 
+    subTitle : String,
+    savedFiles : String, 
+    trustedAdvisors : String,
+    successLogin : String,
+    LLPMembers : String
+  },
+  quickOverview1 : {
+    title : String, 
+    subTitle : String,
+    videoLink : String
+  }, 
+  quickOverview2 : {
+    title : String, 
+    subTitle : String,
+    videoLink : String
+  },  
+  testimonials: [{
+    name:String,
+    profilePhoto:String,
+    certifications:String,
+    comment:String,
+  }],
+  status: String,
+  createdOn: Date,
+  modifiedOn: Date
+})
+
+
+
+
+module.exports = mongoose.model('customercms', customerCmsSchema)
