1639b8c375b8d573068c990b8ce514a07bba8a1b
thoratprashant <prashant.thorat@arkenea.com>
2019-10-24 19:41:59 +0530

    Working on funeral-modal

--- src/app/views/customer/customer-home/final-wishes/funeral-service-modal/funeral-service-modal.component.ts
+++ src/app/views/customer/customer-home/final-wishes/funeral-service-modal/funeral-service-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { egretAnimations } from 'app/shared/animations/egret-animations';
 
 @Component({
@@ -9,8 +9,20 @@ import { egretAnimations } from 'app/shared/animations/egret-animations';
   animations: egretAnimations
 })
 export class FuneralServiceModalComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
   firstServicesSec = true;
   allServicesSec = false;
+  serviceOther = false;
+  ceremonySec = false;
+  reflectionsSec = false;
+  eulogistSec = false;
+  readingsSec = false;
+  musiciansSec = false;
+  pallbearersSec = false;
+
+
+
   selectAnyOneFormGroup: FormGroup;
   funeralOptions: string[] = [
     'I would prefer a traditional funeral', 
@@ -19,11 +31,25 @@ export class FuneralServiceModalComponent implements OnInit {
     'I do not want a funeral or memorial service'
   ];
 
-  constructor(private fb: FormBuilder,) { }
+  constructor(private fb: FormBuilder,private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.selectAnyOneFormGroup = this.fb.group({
       agencyOversees: new FormControl(''),
+    });
+
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+      otherChecked: [false, Validators.required],
+      leaderChecked: [false, Validators.required],
+      eulogistChecked: [false, Validators.required],
+      reflectionsChecked: [false, Validators.required],
+      readingsChecked: [false, Validators.required],
+      musiciansChecked: [false, Validators.required],
+      pallbearersChecked: [false, Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
     });
 
   }
@@ -41,4 +67,77 @@ export class FuneralServiceModalComponent implements OnInit {
   this.firstServicesSec = true;
   this.allServicesSec = false;
  }
+
+ otherChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.serviceOther = true;
+  }else{
+    this.serviceOther = false;
+  }
+}
+leaderChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.ceremonySec = true;
+  }else{
+    this.ceremonySec = false;
+  }
+}
+eulogistChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.eulogistSec = true;
+  }else{
+    this.eulogistSec = false;
+  }
+}
+reflectionsChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.reflectionsSec = true;
+  }else{
+    this.reflectionsSec = false;
+  }
+}
+readingsChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.readingsSec = true;
+  }else{
+    this.readingsSec = false;
+  }
+}
+musiciansChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.musiciansSec = true;
+  }else{
+    this.musiciansSec = false;
+  }
+}
+pallbearersChange(field) {
+  this.firstFormGroup.get(field).setValue(!this.firstFormGroup.get(field).value)
+  let getOtheVal =  this.firstFormGroup.get(field).value;  
+  if(getOtheVal == true){
+    this.pallbearersSec = true;
+  }else{
+    this.pallbearersSec = false;
+  }
+}
+
+
+
+
+
+
+
+
+
 }
