6dadd10d7e401a4f6661b87c8503b7c910d3f467
thoratprashant <prashant.thorat@arkenea.com>
2019-11-21 17:37:39 +0530

    Completed dashboard basic UI

--- src/app/views/admin/app-blank/app-blank.component.ts
+++ src/app/views/admin/app-blank/app-blank.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { egretAnimations } from 'app/shared/animations/egret-animations';
 
 @Component({
   selector: 'app-blank',
   templateUrl: './app-blank.component.html',
-  styleUrls: ['./app-blank.component.scss']
+  styleUrls: ['./app-blank.component.scss'],
+  animations: egretAnimations
 })
 export class AppBlankComponent implements OnInit {
 
