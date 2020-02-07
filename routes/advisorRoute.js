58d803726af95d9d4bcb798b65004230a637d30e
dinzy17 <dineshpatil@arkenea.com>
2020-02-07 16:08:30 +0530

    uploading on staging server

--- routes/advisorRoute.js
+++ routes/advisorRoute.js
@@ -659,6 +659,62 @@ function professionalsListingOLd(req, res) {
   })
 }
 
+function professionalsListingtest(req, res) {
+
+  let { fields, offset, query, order, limit, search,searchString,extraQuery } = req.body;//console.log("limit >>> ",limit)
+  let totalUsers = 0
+
+  if(search && searchString && searchString.trim() != "") {
+    const regSearch = new RegExp(searchString , "i")
+      var firstName = regSearch;
+      var lastName = regSearch;
+      var explode = searchString.split(" ");
+     
+      if(explode[0]){
+        firstName = new RegExp(explode[0] , "i");
+      }
+      if(explode[1]){
+        lastName = new RegExp(explode[1] , "i");
+      }
+    
+      query["$or"] = [
+        { "firstName": firstName },
+        { "lastName": lastName },
+        { "zipcode": regSearch },        
+      ]
+    //  console.log("search",search,"searchString",searchString," query ->",query);
+  }
+  
+  User.aggregate([
+    {
+      $match: {
+        userType: 'advisor',
+      }
+    },
+    {
+      $sort: order
+    },
+    {
+      $lookup:
+        {
+          from: "hired_advisors",
+          localField: "customerId",
+          foreignField: "_id",
+          as: "userInfo"
+        }
+    }
+  ], function (err, logList) {      
+    if (err) {
+      res.status(401).send(resFormat.rError(err))
+    } else {
+      let distanceUserList = usersData;
+      res.send(resFormat.rSuccess({ distanceUserList, totalUsers, "message":"data fetch successfully!"}))
+
+      //res.send(resFormat.rSuccess({ logList, totalRecords }))
+    }
+  }).sort(order).skip(offset).limit(limit)
+}
+
 //function to get list of user as per given criteria
 function professionalsListing(req, res) {
 
@@ -703,7 +759,6 @@ function professionalsListing(req, res) {
               }, 
               "distanceField": 'distance', 
               //"maxDistance": 2000000, 
-              //"query":{"userType": "advisor","status":"Active"},
               "query":query,
               "includeLocs":'coordinates', 
               //"num": 20, 
