22cff5c382c87f3bb14ef120a18b8599a2280824
dinzy17 <dineshpatil@arkenea.com>
2020-02-07 16:34:14 +0530

    uploading on staging server

--- routes/advisorRoute.js
+++ routes/advisorRoute.js
@@ -746,6 +746,7 @@ function professionalsListing(req, res) {
     if (err) {
       res.status(500).send(resFormat.rError(err))
     } else {
+      console.log('>>>>>>',getdata.location.longitude,'########',getdata.location)
         if(getdata && getdata.location && getdata.location.longitude!='0' && getdata.location.longitude!=''){
           let location = getdata.location;
           var longitude = parseFloat(location.longitude);
