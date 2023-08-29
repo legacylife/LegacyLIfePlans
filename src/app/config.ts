bb3a689d12194165420a3488f6a1bd9dcf8555cd
Manoj Nemade <manojn@arkenea.com>
2023-08-29 12:49:40 +0530

    Added Google API key

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 let serverUrlEnv= "http://ec2-44-212-224-69.compute-1.amazonaws.com" // "https://legacylifeplans.com"
 let bucketName = "llp-dev";
-let googleauthkey= "AIzaSyA_UFLb71U2E5y_O7F967fwj5KjUfQcz1Q"; 
+let googleauthkey= "AIzaSyDOIjrH80OfNH7O1wwmFADBVyadAGx6wOs"; //LLP-API-key
 let stripePublishablekey = "pk_test_K9i8VTQjzDdEwtjyKLZLLtjA00ukf8cqnk" //sandbox
 export const googleauthenticationkey = googleauthkey;
 export const stripeKey = stripePublishablekey
